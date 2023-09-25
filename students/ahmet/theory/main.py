dc62988446f36c761e42a0a17a8b611f4458c677
axhvn <139577130+axhvn@users.noreply.github.com>
2023-09-25 21:00:08 +0500

    Update main.py

--- students/ahmet/theory/main.py
+++ students/ahmet/theory/main.py
@@ -1,54 +1,56 @@
-# print("hello")  # Выводит на экран строку
-# name = "Ahmet"  # Создаем переменную со значением "Ahmet"
-# print(name)  # Выводит на экран переменную
+# fmt:off
+print("hello")  # Выводит на экран строку
+name = "Ahmet"  # Создаем переменную со значением "Ahmet"
+print(name)  # Выводит на экран переменную
 
-# a = "10"
-# print(a + "10")  # 1010
+a = "10"
+print(a + "10")  # 1010
 
-# print("10" + 10)  # Ошибка
+print("10" + 10)  # Ошибка
 
-# a = False  # логический тип данных
-# print(a)  # False
+a = False  # логический тип данных
+print(a)  # False
 
-# a = True
-# print(a)  # True
+a = True
+print(a)  # True
 
-# print(10 > 5)  # True
+print(10 > 5)  # True
 
-# firstName = input("введи своё имя: ")
-# print("привет, " + firstName + ". Рад знакомству")
+firstName = input("введи своё имя: ")
+print("привет, " + firstName + ". Рад знакомству")
 
-# if 10 < 5 and 10 > 5:
-#     print("hi")
+if 10 < 5 and 10 > 5:
+    print("hi")
 
-# name = input("введи свое имя: ")
-# if name.lower() == "рахман":
-#     print("ООООООО очень классное имя!!")
-# else:
-#     print("фу, имя не понравилось")
+name = input("введи свое имя: ")
+if name.lower() == "рахман":
+    print("ООООООО очень классное имя!!")
+else:
+    print("фу, имя не понравилось")
 
-# number1 = int(input("введите первое число: "))
-# number2 = int(input("введите второе число: "))
-# summa = number1 + number2
-# print(f"{number1} + {number2} = {summa}")
-# print("{} + {} = {}".format(number1, number2, summa))
-# print("{number1} + {number2} = {summa}".format(number1=10, number2=20, summa=10 + 20))
+number1 = int(input("введите первое число: "))
+number2 = int(input("введите второе число: "))
+summa = number1 + number2
+print(f"{number1} + {number2} = {summa}")
+print("{} + {} = {}".format(number1, number2, summa))
+print("{number1} + {number2} = {summa}".format(number1=10, number2=20, summa=10 + 20))
 
-# number1 = int(input("введите первое число: "))
-# number2 = int(input("введите второе число: "))
-# operation = input("что нужно сделать? ")
+number1 = int(input("введите первое число: "))
+number2 = int(input("введите второе число: "))
+operation = input("что нужно сделать? ")
 
-# if operation == "+":
-#     print(f"{number1} + {number2} = {number1 + number2}")
-# elif operation == "-":
-#     print(f"{number1} - {number2} = {number1 - number2}")
-# elif operation == "*":
-#     print(f"{number1} * {number2} = {number1 * number2}")
-# elif operation == "/":
-#     print(f"{number1} / {number2} = {number1 / number2}")
-# elif operation == "**":
-#     print(f"{number1} ** {number2} = {number1 ** number2}")
-# elif operation == "//":
-#     print(f"{number1} ** {number2} = {number1 // number2}")
-# else:
-#     print(f"{number1} ** {number2} = {number1 % number2}")
+if operation == "+":
+    print(f"{number1} + {number2} = {number1 + number2}")
+elif operation == "-":
+    print(f"{number1} - {number2} = {number1 - number2}")
+elif operation == "*":
+    print(f"{number1} * {number2} = {number1 * number2}")
+elif operation == "/":
+    print(f"{number1} / {number2} = {number1 / number2}")
+elif operation == "**":
+    print(f"{number1} ** {number2} = {number1 ** number2}")
+elif operation == "//":
+    print(f"{number1} ** {number2} = {number1 // number2}")
+else:
+    print(f"{number1} ** {number2} = {number1 % number2}")
+# fmt:on
