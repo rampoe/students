ffecfbda333d1eadd721a31ef9d815a38904c08d
Rahman Begjanov <mail.rampoe@gmail.com>
2023-09-25 14:02:48 +0500

    news

--- students/ahmet/hw_attempt/main.py
+++ students/ahmet/hw_attempt/main.py
@@ -1,34 +1,36 @@
-t = int(input())
-if t == 2:
-    a = int(input())
-    b = int(input())
-    h = input()
-    if h == "+":
-        print(a + b)
-    elif h == "-":
-        print(a - b)
+# fmt:off
+t=int(input())
+if t==2:
+    a=int(input())
+    b=int(input())
+    h=input()
+    if h=='+':
+        print(a+b)
+    elif h=='-':
+        print(a-b)
     else:
-        print(a / b)
-elif t == 3:
-    a = int(input())
-    b = int(input())
-    c = int(input())
-    h = input()
-    if h == "+":
-        print(a + b + c)
-    elif h == "-":
-        print(a - b - c)
+        print(a/b)
+elif t==3:
+    a=int(input())
+    b=int(input())
+    c=int(input())
+    h=input()
+    if h=='+':
+        print(a+b+c)
+    elif h=='-':
+        print(a-b-c)
     else:
-        print(a / b / c)
-elif t == 4:
+        print(a/b/c)
+elif t==4:
     a = int(input())
     b = int(input())
     c = int(input())
     d = int(input())
-    h = input()
-    if h == "+":
-        print(a + b + c + d)
-    elif h == "-":
-        print(a - b - c - d)
+    h=input()
+    if h=='+':
+        print(a+b+c+d)
+    elif h=='-':
+        print(a-b-c-d)
     else:
-        print(a / b / c / d)
+        print(a/b/c/d)
+# fmt:on
