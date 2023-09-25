b3842574c0b6143ad059e54bccc2664295128632
Rahman Begjanov <mail.rampoe@gmail.com>
2023-09-25 14:10:21 +0500

    initial commit

---
+++ students/ahmet/hw_solution/main.py
@@ -0,0 +1,31 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+mainObj = []
+
+c = int(input("enter the amount of numbers: "))
+if c == 2:
+    n1 = int(input("enter 1st number: "))
+    n2 = int(input("enter 2nd number: "))
+if c == 3:
+    n1 = int(input("enter 1st number: "))
+    n2 = int(input("enter 2nd number: "))
+    n3 = int(input("enter 3rd number: "))
+if c == 4:
+    n1 = int(input("enter 1st number: "))
+    n2 = int(input("enter 2nd number: "))
+    n3 = int(input("enter 3rd number: "))
+    n4 = int(input("enter 4th number: "))
