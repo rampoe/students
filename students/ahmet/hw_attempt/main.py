668734429bd7b6f0e147b60c74d0def1353101dd
Rahman Begjanov <mail.rampoe@gmail.com>
2023-09-25 13:59:16 +0500

    initial commit

---
+++ students/ahmet/hw_attempt/main.py
@@ -0,0 +1,34 @@
+t = int(input())
+if t == 2:
+    a = int(input())
+    b = int(input())
+    h = input()
+    if h == "+":
+        print(a + b)
+    elif h == "-":
+        print(a - b)
+    else:
+        print(a / b)
+elif t == 3:
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    h = input()
+    if h == "+":
+        print(a + b + c)
+    elif h == "-":
+        print(a - b - c)
+    else:
+        print(a / b / c)
+elif t == 4:
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    h = input()
+    if h == "+":
+        print(a + b + c + d)
+    elif h == "-":
+        print(a - b - c - d)
+    else:
+        print(a / b / c / d)
