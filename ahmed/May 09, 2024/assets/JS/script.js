28472a8ff387107f5b40d412c2af75fe3acf2d56
rampoe <mail.rampoe@gmail.com>
2024-05-10 19:37:50 +0500

    sync

---
+++ ahmed/May 09, 2024/assets/JS/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let ahmedAge = 14;
+let firstName = "Ahmed";
+
+let ayhanAge;
+let secondName;
+
+secondName = "Vellatov";
+
+ayhanAge = ahmedAge;
+
+alert(ayhanAge);
+
+let a = 12, b = 14, c = 16;
