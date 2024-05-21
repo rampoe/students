59dd891db2e1c605cf0adce41056dab58a972e8e
rampoe <mail.rampoe@gmail.com>
2024-05-21 19:56:56 +0500

    sync

---
+++ ahmed/May 14, 2024/assets/js/script.js
@@ -0,0 +1,69 @@
+"use strict";
+
+// // number
+// 1
+// 2
+// 3
+// -4
+// -65.04
+
+// // string
+// "Ahmed"
+// "I love BTS"
+// "Ayhannnnn"
+
+// // bool
+// true
+// false
+
+// // null
+// null
+
+// // undefined
+// undefined
+
+// let name = null;
+
+// alert(name);
+
+// let firstName = "Ahmed";
+
+// let firstName;
+// alert(firstName);
+// firstName = "Ahmed";
+// alert(firstName);
+
+// let firstName;
+// firstName = "Rahman";
+// firstName = undefined;
+// alert(firstName);
+
+
+
+
+// let num = 700;
+
+// alert(num);
+// alert( typeof(num) );
+
+
+
+
+// let something = "Слава СССР!";
+// alert(typeof something);
+
+
+
+// let something = 4 > 2;
+
+// alert(typeof something);
+
+
+// let something = null;
+
+// alert(typeof something);
+
+
+let something = undefined;
+
+alert(typeof something);
