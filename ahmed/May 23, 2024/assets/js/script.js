d8ddbdb3da09ba0fb54b727f9f99826e18b2cb3f
rampoe <mail.rampoe@gmail.com>
2024-05-23 20:09:46 +0500

    sync

---
+++ ahmed/May 23, 2024/assets/js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// // prompt - аналог input из Python
+// let a = prompt("How old are you?", 20);
+
+// let b = confirm("Are you older than 18?");
+// alert(b); // здесь выведется либо true, либо false
+
+// let c = "10" - "8";
+// alert(c);
+
+// alert(100);
+
+// let n = 999;
+// alert(typeof n);
+
+// let t = 888;
+// alert(typeof t);
+// let s = String(t);
+// alert(s);
+
+// let container = 17;
+// alert(container);
+// container = String(container);
+// alert(container);
+
+// let x = true;
+// alert(typeof x);
+// x = String(x);
+// alert(typeof x);
+
+// let x = "10";
+// let y = "20";
+// x = Number(x);
+// y = Number(y);
+// alert(x + y);
+
+// let x = "ahmed";
+// x = Number(x);
+// alert(x);
+
+// let x = "12976a98745729123756";
+// x = Number(x);
+// alert(typeof x);
+
+// let x = "                               9879087                                ";
+// x = Number(x);
+// alert(x);
