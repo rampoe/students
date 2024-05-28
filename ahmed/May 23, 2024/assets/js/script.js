4f0a798d09f2db044c5884d06d032f7aa260521b
rampoe <mail.rampoe@gmail.com>
2024-05-28 20:09:44 +0500

    sync

--- ahmed/May 23, 2024/assets/js/script.js
+++ ahmed/May 23, 2024/assets/js/script.js
@@ -46,3 +46,38 @@
 // let x = "                               9879087                                ";
 // x = Number(x);
 // alert(x);
+
+// alert(Number(undefined));
+// alert(Number(null));
+// alert(Number(true));
+// alert(Number(false));
+// alert(Number(""));
+
+// console.log(Boolean("")); // true   -    false
+// console.log(Boolean(null)); // false
+// console.log(Boolean("AHMED")); // false   -   true
+// console.log(Boolean("0")); // true
+// console.log(Boolean(" ")); // true
+// console.log(Boolean(0)); // true   -    false
+// console.log(Boolean(1)); // true
+// console.log(Boolean("true")); // false   -   true
+// console.log(Boolean("false")); // false   -    true
+// console.log(Boolean(undefined)); // false
+// console.log(Boolean(NaN)); // false
+// console.log(Boolean(1 / 0)); //false   -    true
+// console.log(Boolean(0 / 128736827633876)); // false
+// console.log(Boolean(10000000000))
+// console.log(Boolean(-100000000000))
+
+
+// let a = prompt("how much $$$$")
+// a = Number(a)
+// alert(`${a} money you have and you will have ${a + 100} if you earn 100manats`  )
+
+
+// let a = prompt('a:');
+// let b = prompt('b:');
+// b= Number(b);
+// a= Number(a);
+// alert(`S = ${a * b}`);
+// alert(`P = ${2 * (a+b)}`);
