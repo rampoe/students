c09568a480b60a555d5124847f8cbbcfbc072c55
rampoe <mail.rampoe@gmail.com>
2024-06-01 18:45:40 +0500

    sync

---
+++ ahmed/May 30, 2024/assets/js/script.js
@@ -0,0 +1,74 @@
+"use strict";
+
+// Превращение строки в число
+
+// let s = "123";
+// let n = Number(s);
+// alert(typeof n);
+
+// let s = "123";
+// let n = +s;
+// alert(typeof n);
+
+// Превращение boolean в number
+
+// let b = true;
+// let n = Number(b);
+// alert(n);
+
+// let b = true;
+// let n = +b;
+// alert(n);
+
+
+// Превращение undefined и null в число
+
+// let u = undefined;
+// u = +u;
+// alert(u);
+
+// let x = null;
+// let y = +x;
+// alert(y);
+
+
+// Наш калькулятор, который умеет только складывать, и то только 2 числа
+
+// let n1 = prompt("Введите первое число:");
+// n1 = Number(n1);
+// let n2 = prompt("Введите второе число:");
+// n2 = Number(n2);
+// alert(n1 + n2);
+
+// let n1 = prompt("Введите первое число:");
+// n1 = +n1;
+// let n2 = prompt("Введите второе число:");
+// n2 = +n2;
+// alert(n1 + n2);
+
+// let n1 = Number(prompt("Введите первое число:"));
+// let n2 = Number(prompt("Введите второе число:"));
+// alert(n1 + n2);
+
+// let n1 = +prompt("Введите первое число:");
+// let n2 = +prompt("Введите второе число:");
+// alert(n1 + n2);
+
+// let n1 = prompt("Введите первое число:");
+// let n2 = prompt("Введите второе число:");
+// alert(Number(n1) + Number(n2));
+
+// let n1 = prompt("Введите первое число:");
+// let n2 = prompt("Введите второе число:");
+// alert(+n1 + +n2);
+
+// let n1 = +prompt("Введите первое число:"), n2 = +prompt("Введите второе число:");
+// alert(n1 + n2);
+
+// alert(+prompt("Введите первое число:") + +prompt("Введите второе число"));
+
+// alert(2 + 2 * 2);
+
+// let x = 9 +  10 ** 2 * 2;
+
+// let x = 10 ,d = 23;
