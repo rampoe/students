97c1e0d9f7d27221c9bd3c899c4c634c81dcee2b
rampoe <mail.rampoe@gmail.com>
2024-06-04 20:28:10 +0500

    sync

--- ahmed/June 04, 2024/assets/js/script.js
+++ ahmed/June 04, 2024/assets/js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// let age = +prompt("Enter your age:");
+// if (age >= 18) {
+//   alert("You can use my website.");
+// } else {
+//   alert("You can't use my website.");
+// }
+
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+// if (year == 2015) alert( 'Вы правы!' );
+
+// let year = +prompt("В каком году Ахмед закончит школу?");
+// if (year == 2027) {
+//   alert("Правильно!");
+//   alert("Поздравляю! Ты угадал!");
+// } else alert("Не верно!");
+
+// let num = +prompt("Вvедите число");
+// if (num % 2 == 0) {
+//   alert("Congratulations France won the world cup");
+// } else {
+//   alert("Portugal won the world cup");
+// }
---
+++ ahmed/June 04, 2024/main.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are allowed to use this website.")
+else:
+    print("You can't use this website!")
