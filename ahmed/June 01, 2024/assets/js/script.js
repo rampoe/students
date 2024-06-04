69ecd652dd701608f5979066e1443477a7d5b941
rampoe <mail.rampoe@gmail.com>
2024-06-04 20:02:29 +0500

    sync

--- ahmed/June 01, 2024/assets/js/script.js
+++ ahmed/June 01, 2024/assets/js/script.js
@@ -1,1 +1,54 @@
 "use strict";
+
+// let name = "Rahman";
+
+// alert(name = "Ahmed");  // Ahmed
+// alert(name == "Ahmed");  // true
+
+// let oneIsGreaterThanTwo = 1 > 2;
+// alert(`One is greater than two: ${oneIsGreaterThanTwo}`);
+
+// alert("Слон" > "Муравей");
+// alert("Лимон" < "Лимонад");
+// alert("арбуз" > "Яблоко");
+
+// alert("" < " "); // true
+// alert("" < "0");
+
+
+// alert(true == 1);
+// alert(false == 0);
+
+// let a = 0;
+// alert( Boolean(a) );  // false
+// let b = "0";
+// alert( Boolean(b) );  // true
+// alert(a == b);  // true
+
+// alert( 0 === false );
+
+// alert( Number(null) );
+
+// alert("12" == 12);
+// alert("12" === 12);
+
+// alert(null === undefined);
+
+// alert(null == undefined);
+
+// alert(null == 0);
+// alert(undefined == 0);
+
+// alert(null == null);
+// alert(undefined == undefined);
+
+// alert(null === null);
+// alert(undefined === undefined);
+
+// alert( null > 0 );
+// alert( null == 0 ); 
+// alert( null >= 0);
+
+// alert(undefined > 0);
+// alert(undefined == 0);
+// alert(undefined < 0);
