1498db33093eb47138542382d51050981a3a0e7e
rampoe <mail.rampoe@gmail.com>
2024-06-07 20:01:33 +0500

    sync

--- ahmed/June 06, 2024/assets/js/script.js
+++ ahmed/June 06, 2024/assets/js/script.js
@@ -1,8 +1,14 @@
 "use strict";
 
-let a = prompt("Какое \"официальноe\" название JavaScript?");
-if (a === "ECMAScript") {
-  alert("Верно!");
-} else {
-  alert("Не знаете? ECMAScript!")
-}
+// let a = prompt("Какое \"официальноe\" название JavaScript?");
+
+// if (a === "ECMAScript") {
+//   alert("Верно!");
+// } else {
+//   alert("Не знаете? ECMAScript!")
+// }
+
+let  a = +prompt('give me a number');
+if( a > 0) alert('1');
+else if (a==0) alert('0');
+else alert('-1');
