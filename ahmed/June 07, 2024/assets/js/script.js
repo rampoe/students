0081f789098bfbe264a6a134a351caaa2d81d1ff
rampoe <mail.rampoe@gmail.com>
2024-06-07 20:29:03 +0500

    sync

--- ahmed/June 07, 2024/assets/js/script.js
+++ ahmed/June 07, 2024/assets/js/script.js
@@ -1,2 +1,14 @@
 "use strict";
 
+// // || - ИЛИ
+// alert(true || true);  // true
+// alert(true || false);  // true
+// alert(false || true);  // true
+// alert(false || false);  // false
+
+// // && - И
+// alert(true && true);  // true
+// alert(true && false);  // false
+// alert(false && true);  // false
+// alert(false && false);  // false
+
