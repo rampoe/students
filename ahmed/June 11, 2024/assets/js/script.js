8aab48efa6fa2a4c68f64549353207d2bc1b566e
rampoe <mail.rampoe@gmail.com>
2024-06-11 20:44:36 +0500

    sync

--- ahmed/June 11, 2024/assets/js/script.js
+++ ahmed/June 11, 2024/assets/js/script.js
@@ -1,2 +1,30 @@
 "use strict";
 
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 3;
+// while (i) alert(i--);
+
+// let i = 0;
+// do {
+//   alert(i);
+//   i++;
+// } while (i < 10);
+
+// for (let i = 0; i < 10; i++) {
+//   alert(i);
+// }
+
+// let i;
+// for (i = 0; i < 100; i++) {
+//   console.log(i);
+// }
+
+// let i;
+// for (i = 0; i < 100; i += 2) {
+//   console.log(i);
+// }
