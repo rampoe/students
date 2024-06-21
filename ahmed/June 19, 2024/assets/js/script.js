1e647b9414f1f7f9267b30d9c463fae0ed41be88
rampoe <mail.rampoe@gmail.com>
2024-06-21 20:05:28 +0500

    sync

--- ahmed/June 19, 2024/assets/js/script.js
+++ ahmed/June 19, 2024/assets/js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// function kot() {
+//   alert("Meou");
+// }
+// function wc() {
+//   let b = prompt("Who won WC");
+//   if (b == "Argentina" || b == "Turkmenistan") {
+//     alert("yes");
+//   } else {
+//     alert("no");
+//   }
+// }
+// wc();
