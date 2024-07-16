64ae02d66ca5d8b9b1b4cc2d56d972ea4854d115
rampoe <mail.rampoe@gmail.com>
2024-07-16 19:53:00 +0500

    sync

--- ahmed/June 27, 2024/assets/js/script.js
+++ ahmed/June 27, 2024/assets/js/script.js
@@ -1,21 +1,53 @@
 "use strict";
 
-
 // function add(a, b) {
-  //   return a + b;
+//   return a + b;
 // }
-
 
 // function sqrt(n) {
 //   return n**(1/2);
 // }
 // alert( sqrt(9) + 1 );
 
-
 // let add = (a, b) => {
 //   alert(a + b);
 // }
 // add(4, 5);
 
+// let add = (a, b) => {
+//   return a + b;
+// }
+// alert(add(3,4));
 
 
+// function add(a, b) {
+//   return a + b;
+// }
+// alert(add(3, 4));
+
+
+// let add = (a, b) => a + b;
+// alert(add(3, 4));
+
+
+// let f = (a, b) => {
+//   a = a + 1;
+//   b = b + 2;
+//   return a + b;
+// }
+
+
+// let sqrt=(n)=>{
+//   n = n**(1/2);
+//   return n;
+// }
+// alert(sqrt(81));
+
+
+// let sqrt=(n)=>{n=n**(1/2);return n;};alert(sqrt(81));
+
+// let sqrt = (n) => n ** (1/2);
+// alert(sqrt(81));
+
+// let sqrt = n => n ** (1/2);
+// alert(sqrt(121));
