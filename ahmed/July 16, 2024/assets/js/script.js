06a0928cb1a6c2e0d50683ceb2a4e3a412a921ab
rampoe <mail.rampoe@gmail.com>
2024-07-19 19:54:47 +0500

    sync

--- ahmed/July 16, 2024/assets/js/script.js
+++ ahmed/July 16, 2024/assets/js/script.js
@@ -1,2 +1,4 @@
-"use strict";
+"use strict";  // строгий режим
 
+let $ = "Ahmed";
+alert($);
---
+++ ahmed/July 18, 2024/assets/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let name = prompt("Enter your first name:");
+
+alert("Hi, " + name);
+
+name = prompt("Enter your second name:");
+
+alert("Your surname is " + name);
+
+let first_name;
+let first__name;
+let first___name;
+let first___________name;
+
