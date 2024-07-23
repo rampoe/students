91f8e7f5aa8a73ac615ef09941fa8de8ad33ef2c
rampoe <mail.rampoe@gmail.com>
2024-07-23 20:49:03 +0500

    sync

--- ahmed/July 23, 2024/assets/js/script.js
+++ ahmed/July 23, 2024/assets/js/script.js
@@ -1,10 +1,55 @@
 "use strict";
 
-let genius = {
-  firstName: "David",
-  secondName: "Goggins",
-  age: 33,
-  lovesAnime: true,
-};
+// let genius = {
+//   firstName: "David",
+//   secondName: "Goggins",
+//   age: 33,
+//   lovesAnime: true,
+// };
 
-alert(`${genius.firstName} ${genius.secondName}`);
+// alert(`${genius.firstName} ${genius.secondName}`);
+
+// let teacher = {
+//   firstName: "Rahman",
+//   age: 18,
+//   likesCoding: true,
+//   isDog: false,
+
+//   introduce: function() {
+//     alert(`Привет всем! Меня зовут Рахман. Мне 18. Люблю программирование.`);
+//   },
+// };
+
+// teacher.introduce();
+
+// let student = {
+//   firstName: "Ahmed",
+//   age: 14,
+//   lovesRyndach: false,
+//   bazaArsena: function () {
+//     alert("Vsem privet ya Ahmed");
+//   },
+// };
+
+// student.bazaArsena();
+
+// function bazaArsena() {
+//   alert("Всем привет я Арсен, раздаю базу, иногда, а иногда нет.");
+// }
+
+// student.age;
+
+// let firstName = "Rahman";
+
+// firstName.length;
+
+// let studentName = "ahmed vellatov";
+// studentName = studentName.toUpperCase();
+// alert(studentName);
+
+// let scream = "WHERE IS MY MONEYYYYY????!!!";
+// function dontScream(scream) {
+//   return scream.toLowerCase();
+// }
+// alert(dontScream(scream));
+
