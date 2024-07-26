8c56e8bcfd9e21c23efc1423de8ac28f22766c52
rampoe <mail.rampoe@gmail.com>
2024-07-26 20:39:45 +0500

    sync

---
+++ ahmed/July 26, 2024/rahman/assets/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// let a = [1, 2, 3];
+
+// let studentsArr = ["Ahmed", "Ayhan", "Eziz", "Ali", "Sirmyrat"];
+
+// for (let i = 0; i < studentsArr.length; i++) {
+//   alert( studentsArr[i] );
+// }
+
+// let studentObj = {
+//   name: "Ahmed",
+//   age: 14,
+//   languages: ["HTML", "CSS", "Bootstrap", "JavaScript", "Python"],
+// };
+
+// alert( studentObj.name );  // Ahmed
+// alert( studentsArr[2] );  // Eziz
+
+// alert("Сейчас я покажу вам всех своих учеников...");
+// for (let i = 0; i < studentsArr.length; i++) {
+//   const element = studentsArr[i];
+//   alert(element);
+// }
+
+// let numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   alert(numbers[i]);
+// }
