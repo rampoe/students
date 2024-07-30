b1ea47d1da12dc0199d21b5c1a86ae7ef6c6456e
rampoe <mail.rampoe@gmail.com>
2024-07-30 20:50:30 +0500

    sync

--- ahmed/Jul 30, 2024/assets/js/script.js
+++ ahmed/Jul 30, 2024/assets/js/script.js
@@ -1,6 +1,18 @@
 "use strict";
 
-let language=["HTML","CSS","PYTHO","J"];
-for(let i=0;i<language.length;i++){
-  alert(language[i]);
-}
+// let language=["HTML","CSS","PYTHO","J"];
+// for(let i=0;i<language.length;i++){
+//   alert(language[i]);
+// }
+
+// function SayHi(){
+//   alert('hi')
+// }
+
+// let s = '1000';
+// let n = Number(s);
+// alert(`${n} ${typeof(n)}`)
+
+// for(let i=100;i>0;i--){
+//   alert(i);
+// };
