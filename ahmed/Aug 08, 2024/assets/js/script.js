fa352b7192fd64fbd67167c4610704529af1bf10
rampoe <mail.rampoe@gmail.com>
2024-08-08 21:25:15 +0500

    sync

--- ahmed/Aug 08, 2024/assets/js/script.js
+++ ahmed/Aug 08, 2024/assets/js/script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+// let p = document.getElementById("p");
+
+// let p = document.querySelector("#p");
+// p.style.color = "green";
+
+// let redBigText = document.querySelector(".red-big-text");
+// redBigText.style.color = "red";
+// redBigText.style.fontSize = "30px";
+
+// let container = document.querySelector("#container p");
+// container.style.color = "blue";
+
+// let big = document.querySelectorAll(".big");
+
+
+// console.log(big); 
+
+// big.style.fontSize = "60px";
+
+
+// let r =[1,2,3,4,5];
+// for(let i=0 ;  i<r.length;i++ ){
+//   console.log(r[i]);
+// };
+
+// let big = document.querySelectorAll(".big");
+
+// for(let i = 0;i<big.length;i++){
+//   big[i].style.fontSize="60px";
+// };
+
+
+// let a = document.querySelectorAll('.container h2');
+// for(let i = 0 ; i<a.length;i++){
+//   a[i].style.border = '100px black solid';
+// }
+
+
