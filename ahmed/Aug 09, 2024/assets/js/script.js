53625348a51868eaf707626593c6873b85652d77
rampoe <mail.rampoe@gmail.com>
2024-08-09 21:00:20 +0500

    sync

--- ahmed/Aug 09, 2024/assets/js/script.js
+++ ahmed/Aug 09, 2024/assets/js/script.js
@@ -1,1 +1,59 @@
 "use strict";
+
+// let btn = document.querySelector("#btn");
+
+// btn.addEventListener("click", () => {
+//   btn.style.backgroundColor = "red";
+// });
+
+// btn.addEventListener("click", function() {});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let c = 0;
+// let btn =document.querySelector("#btn");
+// btn.addEventListener("click", () => {
+
+
+  // if (btn.classList.contains("red")) {
+  //   btn.classList.remove("red");
+  // } else {
+  //   btn.classList.add("red");
+  // }
+
+  // btn.classList.toggle("red");
+
+  // if (c % 5 == 0) {
+  //   btn.style.backgroundColor = "red";
+  //   c++;
+  // } else if (c % 5 == 1) {
+  //   btn.style.backgroundColor = "blue";
+  //   c++;
+  // }
+
+// })
+
+// document.write("Hello")
+
+
+
+let btn = document.querySelector("#btn");
+let counter = 0;
+
+btn.addEventListener("click", () => {
+  btn.innerHTML = counter;
+  counter++;
+})
