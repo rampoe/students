6c63ea5585928a64b81e0a0b8af33d114b44d6af
rampoe <mail.rampoe@gmail.com>
2024-08-15 20:15:26 +0500

    sync

--- ahmed/Aug 15, 2024/assets/js/script.js
+++ ahmed/Aug 15, 2024/assets/js/script.js
@@ -1,2 +1,8 @@
 "use strict";
 
+// let body = document.querySelector("body");
+// let btn = document.querySelector("#btn");
+// btn.addEventListener("click", () => {
+//   body.style.backgroundColor = "red";
+// });
+
