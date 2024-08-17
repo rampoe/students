a96351792b184a57412db83044ccca626251ee55
rampoe <mail.rampoe@gmail.com>
2024-08-17 16:21:50 +0500

    sync

--- ahmed/Aug 17, 2024/assets/js/script.js
+++ ahmed/Aug 17, 2024/assets/js/script.js
@@ -1,2 +1,22 @@
 "use strict";
 
+let btn = document.querySelector("#btn"),
+    text = document.querySelector("#text");
+
+btn.addEventListener("click", () => {
+  // text.classList.add("red");
+
+  // if (text.style.color === "red") {
+  //   text.style.color = "black";
+  // } else {
+  //   text.style.color = "red";
+  // }
+
+  // if (text.classList.contains("red")) {
+  //   text.classList.remove("red");
+  // } else {
+  //   text.classList.add("red");
+  // }
+
+  text.classList.toggle("red");
+});
